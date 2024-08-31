147c02106ca9b3f11bc9027cf399b466aaed6b5c
Alexander Haberl <azteke42@gmail.com>
2024-08-31 17:11:51 +0200

    chore: fix clippy warning

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 use std::ffi::c_char;
 use std::ffi::CString;
 
-pub fn vec_cstring_to_vec_pointers(vector: &Vec<CString>) -> Vec<*const c_char> {
+pub fn vec_cstring_to_vec_pointers(vector: &[CString]) -> Vec<*const c_char> {
     let vec_raw: Vec<*const c_char> = vector
         .iter()
         .map(|layer_name| layer_name.as_ptr())
